51c74f204f8ee717840d37d8ad33a51e70142597
MarcosCamara01 <marcospenelascamara@gmail.com>
2024-04-23 22:49:17 +0200

    app colors and theme toggle

---
+++ src/components/theme/common/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ThemeToggle } from '../theme-toggle'
+
+const Header = () => {
+    return (
+        <header className='flex mb-5 md:mb-10 items-center'>
+            Marcos Cámara
+            
+            <nav className="font-mono text-xs grow justify-end items-center flex gap-1 md:gap-3">
+                <ThemeToggle />
+            </nav>
+        </header>
+    )
+}
+
+export default Header
