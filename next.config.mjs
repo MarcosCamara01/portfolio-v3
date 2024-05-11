70a47fde2ac76dc2126c8b7277fb787af0131123
MarcosCamara01 <marcospenelascamara@gmail.com>
2024-05-11 19:01:10 +0200

    trying to solve production error

--- next.config.mjs
+++ next.config.mjs
@@ -1,71 +1,59 @@
-import createMDX from '@next/mdx'
+import nextMdx from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-    experimental: {
-        mdxRs: true,
-    },
-    swcMinify: true,
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "pbs.twimg.com",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "abs.twimg.com",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "m.media-amazon.com",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "images-na.ssl-images-amazon.com",
-                port: "",
-                pathname: "/**",
-            },
+
+const withMDX = nextMdx({
+  extension: /\.mdx?$/,
+});
+
+const nextConfig = withMDX({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+  swcMinify: true,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "abs.twimg.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  headers() {
+    return [
+      {
+        source: "/images/rauchg-3d4cecf.jpg",
+        headers: [
+          {
+            key: "cache-control",
+            value: "public, max-age=31536000, immutable",
+          },
         ],
-    },
-    headers() {
-        return [
-            {
-                source: "/images/rauchg-3d4cecf.jpg",
-                headers: [
-                    {
-                        key: "cache-control",
-                        value: "public, max-age=31536000, immutable",
-                    },
-                ],
-            },
-        ];
-    },
-    redirects() {
-        return [
-            {
-                source: "/essays/:nested*",
-                destination: "/",
-                permanent: true,
-            },
-            {
-                source: "/slackin/:nested*",
-                destination: "https://github.com/rauchg/slackin",
-                permanent: true,
-            },
-        ];
-    },
-};
+      },
+    ];
+  },
+});
 
-const withMDX = createMDX({});
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
