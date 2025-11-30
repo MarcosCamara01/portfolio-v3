17f7a9881f9aedbcf847c0c4f13cf3e8d9794fce
Marcos <marcospenelascamara@gmail.com>
2025-11-30 22:49:00 +0100

    Add new article on "use workflow" in Next.js
    - Introduced a comprehensive guide detailing the "use workflow" directive, explaining its functionality, advantages, and practical use cases.
    - Included examples demonstrating how to implement durable workflows in various scenarios, such as user onboarding and payment processing.
    - Discussed the benefits of eliminating the need for additional infrastructure like cron jobs and message queues, emphasizing the simplicity of using JavaScript code for complex workflows.

--- src/components/portfolio/writing.tsx
+++ src/components/portfolio/writing.tsx
@@ -3,8 +3,16 @@ import { P } from '../posts/p';
 import { FiEdit3 } from 'react-icons/fi';
 import { UL } from '../posts/ul';
 import { Li } from '../posts/li';
+import { getPosts } from '@/get-posts';
 
-export const Writing = () => {
+export const Writing = async () => {
+  const posts = await getPosts();
+
+  const getYear = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.getFullYear().toString();
+  };
+
   return (
     <section className="space-y-6">
       <div className="flex items-center gap-4">
@@ -22,16 +30,11 @@ export const Writing = () => {
           experience. Some of my recent articles:
         </P>
         <UL>
-          <Li>
-            <A href="/2025/stop-calling-next-js-slow-master-these-optimization-techniques">
-              Stop Calling Next.js Slow: Master These Optimization Techniques
-            </A>
-          </Li>
-          <Li>
-            <A href="/2024/get-the-best-performance-on-nextjs-app">
-              Get the Best Performance on Your Next.js App
-            </A>
-          </Li>
+          {posts.map((post) => (
+            <Li key={post.id}>
+              <A href={`/${getYear(post.date)}/${post.id}`}>{post.title}</A>
+            </Li>
+          ))}
         </UL>
       </div>
     </section>
